a6b85587782b57081a830367ecf8843bd188c7c5
amirhosseinmirzanezhad <amirhosseinmirzanezhad1001@gmail.com>
2021-12-07 17:53:49 +0330

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function theme(){
+    var dtheme = document.getElementById('bck')
+    dtheme.style.backgroundColor = "black"
+}
+function chname(){
+    var namech = document.getElementById('chname')
+    namech.innerHTML = " >> نام تغییر کرد <<"
+}
+function hidepic(){
+    var mypic = document.getElementById('pic')
+    mypic.style.visibility = "hidden"
+}
+function changepic(){
+    var chpic = document.getElementById('pic')
+    chpic.src = "images/pic2.jpg"
+}
